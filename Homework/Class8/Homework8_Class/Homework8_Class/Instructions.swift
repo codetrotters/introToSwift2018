4b43c699f96374f1c67e02ebc4e515b8a1cd146d
klopez <klopez@wayfair.com>
2018-05-01 23:57:47 -0400

    Clarified a point on homework 8

--- Homework/Class8/Homework8_Class/Homework8_Class/Instructions.swift
+++ Homework/Class8/Homework8_Class/Homework8_Class/Instructions.swift
@@ -10,7 +10,7 @@
 //
 //  Tasks
 //
-//  1) Hurricane Maria struck again and unfrotunately has hit this codebase hard. A bunch of code was erased and altered as a result and the codebase is not compiling. Please help in getting the codebase compiling without warnings or errors :)
+//  1) Hurricane Maria struck again and unfrotunately has hit this codebase hard. A bunch of code was erased and altered as a result and the codebase is not compiling. Please help in getting the codebase compiling without warnings or errors. In addition, the goal here is to get the user experience to match up with what is shown in the video. Pay attention to the details :)
 //
 //  2) Once you get the codebase humming again, make the textfield for security code more secure by adding modifying the 'Secure Text Entry' property.
 //
@@ -18,11 +18,11 @@
 
 //  4) Change the input accessory view of the 'Expiration Date' so that it shows a UIPickerView with two columns of options, one for month and one for year.
 //
-//  5) Modify the table view so that any active keyboard is dismissed when the user drags on the view
+//  5) Modify the table view so that any active keyboard or input accessory view is dismissed when the user drags on the view
 //
 //  6) Add the ability to delete a credit card on the CreditCardsViewController simply by swiping the cell to the left
 //
-//  7) Modify the configuration of the CreditCardSummaryCell so that the nameLabel shows 'FIRST_NAME + LAST_NAME' and so that the cardInfoLabel shows as "***[LAST_FOUR_CARD_NUMBER+DIGITS], expires EXPIRATION_DATE"
+//  7) Modify the configuration of the CreditCardSummaryCell so that the nameLabel shows 'FIRST_NAME LAST_NAME' and so that the cardInfoLabel shows as "***[LAST_FOUR_CARD_NUMBER+DIGITS], expires EXPIRATION_DATE"
 //
 //  8) Add the ability to edit an existing credit card when a user taps on a credit card cell.
 //
