9cd47553bbf25de09f9ab187c7122808bde92044
klopez <klopez@wayfair.com>
2018-05-03 18:33:42 -0400

    Added the pokemon project to the porject folder

---
+++ Projects/OtherClassProjects/Pokemon_KL/Pokemon_KL/Pokemon_KL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon_KL
+//
+//  Created by Kevin Lopez on 5/3/18.
+//  Copyright © 2018 io.ricoLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
